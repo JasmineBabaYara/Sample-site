50e9e57fc9483b3e718015a5aa9d57a78b7b6d9d
JasmineBabaYara <jassyprincess1@gmail.com>
2021-09-26 10:21:56 -0700

    sample-site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+      <div className='container'>
+        <div className='nav-container'>
+          <h1>ARARAT</h1>
+          <p>ARCHITECHTURE</p>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Services</li>
+            <li>Blog
+              <i className="bi bi-caret-down"></i>
+            </li>
+            <li>pages
+              <i className="bi bi-caret-down" ></i>
+            </li>
+            <li>contact</li>
+          </ul>
+          <button className='navbtn'>get in touch</button>
+        </div>
+
+        <img src='/images/build.jpg' alt=''/>
+        <p className='img-text'> we design your space</p>
+        <p className='img-text-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,ulla arcu arcu.</p>
+        <button className='img-btn'>see our projects</button>
+      </div>
+    )
+  
 }
 
 export default App;
